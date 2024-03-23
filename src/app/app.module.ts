46d7ee9d0ff0c09d090672be3230f08fe745f117
stefan-milkovski <stefan.milkovski@generadevelopment.com>
2024-03-23 13:01:17 +0100

    adding all the css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { WeatherComponent } from './components/weather/weather.component';
-import { WeatherService } from './services/weather.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { WeatherComponent } from './components/weather/weather.component';
 import { GeocodeComponent } from './components/geocode/geocode.component';
+import { WeatherService } from './services/weather.service';
 
 @NgModule({
   declarations: [
